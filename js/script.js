0590e527a2fab7e160208e97fcdcd50ef081069f
vinayagaratnamthirulogachandran <misterthiru610@gmail.com>
2023-05-11 21:44:21 +0530

    js finished

--- js/script.js
+++ js/script.js
@@ -2,14 +2,29 @@ const btnStart = document.querySelector('.start');
 const btnStop = document.querySelector('.stop');
 const btnReset = document.querySelector('.reset');
 
-let hrs = min = sec = ms = 0,startTiem;
+let hrs = min = sec = ms = 0,startTime;
 
 btnStart.addEventListener('click', ()=>{
-    
+    startTime=setInterval(()=>{
+        ms++;
+        if(ms==100){
+            sec++;
+            ms=0;
+        }
+        if(sec==60){
+            min++;
+            sec=0;
+        }
+        if(min==60){
+            hrs++;
+            min=0;
+        }
+        updateDisplay();
+    },10);
 });
 
 btnStop.addEventListener('click', ()=>{
-    
+    clearInterval(startTime);
 });
 
 btnReset.addEventListener('click', ()=>{
@@ -18,12 +33,12 @@ btnReset.addEventListener('click', ()=>{
 });
 
 function updateDisplay(){
-    hrs=hrs<10?'0'+hrs:hrs;
-    min=min<10?'0'+min:min;
-    sec=sec<10?'0'+sec:sec;
-    ms=ms<10?'0'+ms:ms;
-    document.querySelector('.hrs').innerText=hrs;
-    document.querySelector('.min').innerText=min;
-    document.querySelector('.sec').innerText=sec;
-    document.querySelector('.ms').innerText=ms;
+    thrs=hrs<10?'0'+hrs:hrs;
+    tmin=min<10?'0'+min:min;
+    tsec=sec<10?'0'+sec:sec;
+    tms=ms<10?'0'+ms:ms;
+    document.querySelector('.hrs').innerText=thrs;
+    document.querySelector('.min').innerText=tmin;
+    document.querySelector('.sec').innerText=tsec;
+    document.querySelector('.ms').innerText=tms;
 }
